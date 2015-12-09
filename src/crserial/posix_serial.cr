9ba6aec3a81f8ccf0ab8ea1594b78520a6caafce
Dmitry Rubinstein <rubinsteindb@gmail.com>
2015-12-09 00:23:22 +0000

    Fixing bugs

--- src/crserial/posix_serial.cr
+++ src/crserial/posix_serial.cr
@@ -3,16 +3,16 @@ class Serial < IO::FileDescriptor
   def initialize(address, baude_rate=9600, data_bits=8)
     file_opts = LibC::O_RDWR | LibC::O_NOCTTY | LibC::O_NONBLOCK
     fd = LibC.open(address, file_opts)
-    raise Errno.new("open") if @fd < 0
+    raise Errno.new("open") if fd < 0
 
-    fl = LibC.fcntl(@fd, LibC::FCNTL::F_GETFL, 0)
+    fl = LibC.fcntl(fd, LibC::FCNTL::F_GETFL, 0)
     raise Errno.new("fcntl") if fl < 0
 
-    err = LibC.fcntl(@fd, LibC::FCNTL::F_SETFL, ~LibC::O_NONBLOCK & fl)
+    err = LibC.fcntl(fd, LibC::FCNTL::F_SETFL, ~LibC::O_NONBLOCK & fl)
     raise Errno.new("fcntl") if err < 0
 
     config = build_config(baude_rate, data_bits)
-    err = LibC.tcsetattr(@fd, LibC::TCSANOW, @config)
+    err = LibC.tcsetattr(fd, LibC::TCSANOW, config)
     raise Errno.new("tcsetattr") if err < 0
     
     super(fd)
